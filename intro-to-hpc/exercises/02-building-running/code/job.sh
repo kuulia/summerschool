fd815a5bb42383ed97fc9dc943870aff62a64453
kuulia <linus.lind00@gmail.com>
2026-06-23 14:31:55 +0300

    intro-02-sol

---
+++ intro-to-hpc/exercises/02-building-running/code/job.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --job-name=test_make
+#SBATCH --account=project_462001452
+#SBATCH --reservation=SummerSchoolCPU
+#SBATCH --partition=small
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=1G
+#SBATCH --time=00:05:00
+
+srun prog.x
