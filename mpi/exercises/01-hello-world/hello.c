fdfffcea09c354a2ab615058ddb67b4448c35d9c
kuulia <linus.lind00@gmail.com>
2026-06-24 10:31:22 +0300

    add rank and size decl

--- mpi/exercises/01-hello-world/hello.c
+++ mpi/exercises/01-hello-world/hello.c
@@ -1,18 +1,15 @@
-/*
- * SPDX-FileCopyrightText: 2010 CSC - IT Center for Science Ltd. <www.csc.fi>
- *
- * SPDX-License-Identifier: MIT
- */
-
 #include <stdio.h>
 #include <mpi.h>
 
 int main(int argc, char *argv[]) {
+    int rank, size;
 
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    // TODO: say hello! in parallel
-    printf("Hello!\n");
+    printf("Hello from rank %d of %d!\n", rank, size);
+
+    MPI_Finalize();
+    return 0;
 }
