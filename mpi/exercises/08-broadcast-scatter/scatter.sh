46e74572418ed8861910a56e6332a7975ba9de72
kuulia <linus.lind00@gmail.com>
2026-06-25 15:46:56 +0300

    add job sh

---
+++ mpi/exercises/08-broadcast-scatter/scatter.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name=mpi_scatter
+#SBATCH --account=project_462001452
+#SBATCH --reservation=SummerSchoolCPU
+#SBATCH --partition=small
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=64
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=1G
+#SBATCH --time=00:05:00
+
+# Run the MPI program
+srun ./skeleton_scatter
+
