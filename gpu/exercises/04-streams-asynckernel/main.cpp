a4556003f9bf28ef639f28b615e5d7d356b3559b
kuulia <linus.lind00@gmail.com>
2026-06-29 11:37:04 +0300

    async

--- gpu/exercises/04-streams-asynckernel/main.cpp
+++ gpu/exercises/04-streams-asynckernel/main.cpp
@@ -6,18 +6,19 @@
  * This code in its current form uses the default stream
  * Task:
  * - Place kernel_{a,b,c} in separate streams and execute them asynchronously
- * - Validate that kernels are executing concurrently with `run_tue ... rocprof --hip-trace ./<your->
- *   - Open chromium url chrome://tracing or https://ui.perfetto.dev, open file "results.json"
+ * - Validate that kernels are executing concurrently with `run_tue ... rocprof
+ * --hip-trace ./<your->
+ *   - Open chromium url chrome://tracing or https://ui.perfetto.dev, open file
+ * "results.json"
  */
 
+#include "error_checking.hpp"
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
-#include "error_checking.hpp"
 
 // GPU kernel definition
-__global__ void kernel_a(float *a, int n)
-{
+__global__ void kernel_a(float *a, int n) {
   int tid = threadIdx.x + blockIdx.x * blockDim.x;
 
   // Evaluate the trigonometric identity
@@ -32,9 +33,7 @@ __global__ void kernel_a(float *a, int n)
   }
 }
 
-
-__global__ void kernel_b(float *b, int n)
-{
+__global__ void kernel_b(float *b, int n) {
   int tid = threadIdx.x + blockIdx.x * blockDim.x;
 
   // Heavy kernel: repeatedly updates x using sine, cosine, and arctangent
@@ -50,8 +49,7 @@ __global__ void kernel_b(float *b, int n)
   }
 }
 
-__global__ void kernel_c(float *c, int n)
-{
+__global__ void kernel_c(float *c, int n) {
   int tid = threadIdx.x + blockIdx.x * blockDim.x;
 
   if (tid < n) {
@@ -69,67 +67,84 @@ __global__ void kernel_c(float *c, int n)
 }
 
 int main() {
-  constexpr size_t N = 1<<24; // ~64 MiB array
+  constexpr size_t N = 1 << 24; // ~64 MiB array
 
   constexpr int blocksize = 256;
-  constexpr int gridsize =(N-1+blocksize)/blocksize;
-  constexpr size_t N_bytes = N*sizeof(float);
+  constexpr int gridsize = (N - 1 + blocksize) / blocksize;
+  constexpr size_t N_bytes = N * sizeof(float);
 
   // Host & device pointers
-  float *a; float *d_a;
-  float *b; float *d_b;
-  float *c; float *d_c;
+  float *a;
+  float *d_a;
+  float *b;
+  float *d_b;
+  float *c;
+  float *d_c;
 
   // Host allocations
-  a = (float*) malloc(N_bytes);
-  b = (float*) malloc(N_bytes);
-  c = (float*) malloc(N_bytes);
+  a = (float *)malloc(N_bytes);
+  b = (float *)malloc(N_bytes);
+  c = (float *)malloc(N_bytes);
 
-  #error create three separate streams
+  // create three separate streams
+  hipStream_t stream_a, stream_b, stream_c;
 
   // Device allocations
-  HIP_ERRCHK(hipMalloc((void**)&d_a, N_bytes));
-  HIP_ERRCHK(hipMalloc((void**)&d_b, N_bytes));
-  HIP_ERRCHK(hipMalloc((void**)&d_c, N_bytes));
+  HIP_ERRCHK(hipMalloc((void **)&d_a, N_bytes));
+  HIP_ERRCHK(hipMalloc((void **)&d_b, N_bytes));
+  HIP_ERRCHK(hipMalloc((void **)&d_c, N_bytes));
 
   // warmup
   kernel_c<<<gridsize, blocksize>>>(d_a, N);
-  HIP_ERRCHK(hipMemcpy(a, d_a, N_bytes/100, hipMemcpyDefault));
+  HIP_ERRCHK(hipMemcpy(a, d_a, N_bytes / 100, hipMemcpyDefault));
   HIP_ERRCHK(hipDeviceSynchronize());
 
   // Execute kernels in sequence
-  #error Launch each kernel in a different stream
-  kernel_a<<<gridsize, blocksize,0,0>>>(d_a, N);
+
+  HIP_ERRCHK(hipMemcpyAsync(d_a, a, N_bytes, hipMemcpyHostToDevice, stream_a));
+  HIP_ERRCHK(hipMemcpyAsync(d_b, b, N_bytes, hipMemcpyHostToDevice, stream_b));
+  HIP_ERRCHK(hipMemcpyAsync(d_c, c, N_bytes, hipMemcpyHostToDevice, stream_c));
+  // Launch each kernel in a different stream
+  kernel_a<<<gridsize, blocksize, 0, stream_a>>>(d_a, N);
   HIP_ERRCHK(hipGetLastError());
 
-  kernel_b<<<gridsize, blocksize,0,0>>>(d_b, N);
+  kernel_b<<<gridsize, blocksize, 0, stream_b>>>(d_b, N);
   HIP_ERRCHK(hipGetLastError());
 
-  kernel_c<<<gridsize, blocksize,0,0>>>(d_c, N);
+  kernel_c<<<gridsize, blocksize, 0, stream_c>>>(d_c, N);
   HIP_ERRCHK(hipGetLastError());
 
   // Copy results back (in the default stream with hipMemCpy)
-  #error synchronize the host with stream A, before copying d_A back
-  HIP_ERRCHK(hipMemcpy(a, d_a, N_bytes, hipMemcpyDefault));
-  #error synchronize the host with stream B, before copying d_B back
-  HIP_ERRCHK(hipMemcpy(b, d_b, N_bytes, hipMemcpyDefault));
-  #error synchronize the host with stream C, before copying d_C back
-  HIP_ERRCHK(hipMemcpy(c, d_c, N_bytes, hipMemcpyDefault));
+  // #error synchronize the host with stream A, before copying d_A back
+  hipStreamSynchronize(stream_a);
+  HIP_ERRCHK(hipMemcpyAsync(a, d_a, N_bytes, hipMemcpyDeviceToHost, stream_a));
+  // #error synchronize the host with stream B, before copying d_B back
+  hipStreamSynchronize(stream_b);
+  HIP_ERRCHK(hipMemcpyAsync(b, d_b, N_bytes, hipMemcpyDeviceToHost, stream_b));
+  // #error synchronize the host with stream C, before copying d_C back
+  hipStreamSynchronize(stream_c);
+  HIP_ERRCHK(hipMemcpyAsync(c, d_c, N_bytes, hipMemcpyDeviceToHost, stream_c));
 
-  for (int i = 0; i < 10; ++i) printf("%f ", a[i]);
+  for (int i = 0; i < 10; ++i)
+    printf("%f ", a[i]);
   printf("\n");
 
-  for (int i = 0; i < 10; ++i) printf("%f ", b[i]);
+  for (int i = 0; i < 10; ++i)
+    printf("%f ", b[i]);
   printf("\n");
 
-  for (int i = 0; i < 10; ++i) printf("%f ", c[i]);
+  for (int i = 0; i < 10; ++i)
+    printf("%f ", c[i]);
   printf("\n");
   // Free device and host memory allocations
   HIP_ERRCHK(hipFree(d_a));
   HIP_ERRCHK(hipFree(d_b));
   HIP_ERRCHK(hipFree(d_c));
 
-  #error destroy all streams
+  // destroy all streams
+  HIP_ERRCHK(hipStreamDestroy(stream_a));
+  HIP_ERRCHK(hipStreamDestroy(stream_b));
+  HIP_ERRCHK(hipStreamDestroy(stream_c));
 
   free(a);
   free(b);
